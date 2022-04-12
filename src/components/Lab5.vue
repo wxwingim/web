0e9716d2d59f89217dff677843f08be1eeca63e8
wxwingim <78482070+wxwingim@users.noreply.github.com>
2022-04-12 19:33:20 +0300

    Add files via upload

---
+++ src/components/Lab5.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <section>
+            <Lab5Table
+                :users_data="USERS"
+            />
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+import { defineComponent } from 'vue';
+import Lab5Table from './Lab5Table.vue';
+import {mapActions, mapGetters} from 'vuex';
+
+export default defineComponent({
+
+    components:{
+        Lab5Table
+    },
+
+    data: () => {
+        return{
+
+        }
+    },
+
+    computed:{
+        ...mapGetters([
+            'USERS'
+        ])
+    },
+
+    methods:{
+        ...mapActions([
+            'GET_ALL_USERS'
+        ])
+    },
+
+    mounted(){
+        this.GET_ALL_USERS();
+    }
+});
+</script>
+
+<style scope>
+</style>
